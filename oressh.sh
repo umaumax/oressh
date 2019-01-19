11acad9b16d0e5b59eca816c314212c5b7e2da30
umaumax <umauma7083@gmail.com>
2019-01-19 15:17:51 +0900

    Change base64 command to python version

--- oressh.sh
+++ oressh.sh
@@ -6,18 +6,25 @@ shopt -s expand_aliases
 function oressh() {
 	[[ $# -le 0 ]] && echo "$0 hostname" && exit 1
 	local host=$1
-	if [[ $(uname) == "Darwin" ]]; then
-		alias local_base64encode='base64'
-		alias local_base64decode='base64 -D'
-	elif [[ $(uname -a) =~ "Ubuntu" ]]; then
-		alias local_base64encode='base64 -w 0'
-		alias local_base64decode='base64 -d'
-	else
-		echo 'no support os'
-		return 1
-	fi
 
-	local remote_base64decode="if [[ \$(uname) == Darwin ]]; then base64 -D; else base64 -d; fi"
+	# NOTE: python base64 version
+	local remote_base64decode="python -c \"import base64; import sys; sys.stdout.write(base64.b64decode(sys.stdin.read()).decode(\\\"utf-8\\\"))\""
+	alias local_base64encode="python -c \"import base64; import sys; sys.stdout.write(base64.b64encode(sys.stdin.buffer.read()).decode('utf-8'))\""
+	alias local_base64decode="$remote_base64decode"
+
+	# NOTE: base64 command version
+	# 	if [[ $(uname) == "Darwin" ]]; then
+	# 		alias local_base64encode='base64'
+	# 		alias local_base64decode='base64 -D'
+	# 	elif [[ $(uname -a) =~ "Ubuntu" ]]; then
+	# 		alias local_base64encode='base64 -w 0'
+	# 		alias local_base64decode='base64 -d'
+	# 	else
+	# 		echo 'no support os'
+	# 		return 1
+	# 	fi
+	# 	local remote_base64decode="if [[ \$(uname) == Darwin ]]; then base64 -D; else base64 -d; fi"
+
 	# NOTE: bash-3.2ではなぜか正常な動作とならないので，macの場合には無理やりlocalの方のbashを参照
 	# NOTE: sh cannot parse $(() || ()) or [[ ]], and can't use process replace <(), so wrap entire command as bash
 	# NOTE: したがって，bashでwrapしている
