fd69d6d4a11f086788098c90d0946c95667cef30
umaumax <umauma7083@gmail.com>
2019-01-24 19:24:56 +0900

    Update to version 0.1.0

--- README.md
+++ README.md
@@ -1,31 +1,37 @@
 # oressh
 
-oreore ssh command
+* oreore ssh command
+  * create oreore environment on remote machine with minimal sideeffect
+
+## how to use
+```
+oressh [host]
+```
+
+## settings
+```
+~/.config/oressh/{default.$host_name,...}/{.bashrc,.inputrc,.vimrc}
+```
 
 ## NOTE
 * ssh先の環境を汚さずにoreore bashとoreore vimの設定を利用する
 * functionやaliasではsshpassから使用できないため，コマンドとして提供
 * defaultでpythonを利用して，base64のencodeとdecodeを行う(at both local host and remote host)
   * env `NO_PYTHON_BASE64`に何かしらの値を設定すると`base64`コマンドを利用する
+* process substitutionが使用できない環境に限り，`ln -s /proc/self/fd /dev/fd`を実行するが，それ以外では，設定ファイルは作成しない
+* hostの接続先によって、設定ファイルを選択する(読み込ませたくないファイルがある場合には，同名のファイルをそのhostに対応する場所に作成する)
+  * `~/.config/oressh/default/`: 共通の処理
+  * `~/.config/oressh/$host_name/`: each host
 
 ## TODO
 * write help command
 * READMEの充実
-* readline for bash
-  * ~/.inputrcの設定も行いたいが，物理的にファイルを配置する必要性がありそうなので，副作用がある
-* hostの接続先によって、設定ファイルを選択する
-  * `~/.config/oressh/dotfiles/default/.bashrc`: 対応するhost_nameのdirがない場合
-  * `~/.config/oressh/dotfiles/common/.bashrc`: 共通の処理
-  * `~/.config/oressh/dotfiles/host_name/.bashrc`: each host
 
 ### for wgit
 ```
 https://github.com/umaumax/oressh/blob/master/oressh.sh oressh
 chmod u+x oressh
 ```
-
-## Required
-* [umaumax/dotfiles]( https://github.com/umaumax/dotfiles )
 
 ## FYI
 [シェル芸で、ローカルにあるbashrcやvimrcをssh接続先で\(ファイルをコピーとかせずに\)利用する \| 俺的備忘録 〜なんかいろいろ〜]( https://orebibou.com/2018/10/%E3%82%B7%E3%82%A7%E3%83%AB%E8%8A%B8%E3%81%A7%E3%80%81%E3%83%AD%E3%83%BC%E3%82%AB%E3%83%AB%E3%81%AB%E3%81%82%E3%82%8Bbashrc%E3%82%84vimrc%E3%82%92ssh%E6%8E%A5%E7%B6%9A%E5%85%88%E3%81%A7%E3%83%95/ )
--- oressh.sh
+++ oressh.sh
@@ -3,10 +3,25 @@
 # for alias (for bash)
 shopt -s expand_aliases
 
-mkdir -p ~/.config/oressh/
+mkdir -p ~/.config/oressh/default/
+
+function cat_if_exist() {
+	local filepath=$1
+	[[ -f $filepath ]] && cat $filepath && return 0
+	return 1
+}
+
+function find_exist_file() {
+	for filepath in "$@"; do
+		[[ -f "$filepath" ]] && echo "$filepath" && return 0
+	done
+	return 1
+}
 
 function oressh() {
-	[[ $# -le 0 ]] && echo "$0 [hostname]" && exit 1
+	[[ $# -lt 1 ]] && echo "$0 [hostname]" && exit 1
+	[[ $# -gt 1 ]] && echo "$0 [hostname]" && exit 1
+
 	local host=$1
 
 	# NOTE: python base64 version
@@ -35,15 +50,29 @@ function oressh() {
 	# NOTE: 注意点として，shoptからlogin shellではないことになっている
 	local bash_cmd='$( ( [ -e /usr/local/bin/bash ] && echo /usr/local/bin/bash ) || ( [ -e /bin/bash ] && echo /bin/bash ) )'
 
+	local inputrc_filepath=$(find_exist_file "$HOME/.config/oressh/$host/.inputrc" "$HOME/.config/oressh/default/.inputrc")
+	local vimrc_filepath=$(find_exist_file "$HOME/.config/oressh/$host/.vimrc" "$HOME/.config/oressh/default/.vimrc")
+
 	# FYI: [How to fix the /dev/fd/63: No such file or directory? – Site Title]( https://jaredsburrows.wordpress.com/2014/06/25/how-to-fix-the-devfd63-no-such-file-or-directory/ )
 	# NOTE: This command has side effect
 	local enable_process_substitution_cmd='[[ $USER == "root" ]] && [[ ! -e /dev/fd ]] && ln -s /proc/self/fd /dev/fd'
 	ssh -t -t $host "$enable_process_substitution_cmd; bash -c '$bash_cmd --rcfile <( echo -e " \
 		$({
-			cat <(echo 'function vim() { command vim -u <(echo '$(cat ~/dotfiles/.minimal.vimrc | local_base64encode)' | '$remote_base64decode') $@ ; }')
-			cat ~/dotfiles/.minimal.bashrc
+			# .vimrc
+			[[ -n $vimrc_filepath ]] && echo 'type vim >/dev/null 2>&1 && function vim() { command vim -u <(echo '$(cat ~/dotfiles/.minimal.vimrc | local_base64encode)' | '$remote_base64decode') $@ ; }'
+			[[ -n $vimrc_filepath ]] && echo 'type vi  >/dev/null 2>&1 && function vi()  { command vi -u <(echo '$(cat ~/dotfiles/.minimal.vimrc | local_base64encode)' | '$remote_base64decode') $@ ; }'
+			# .inputrc
+			# NOTE: cat wrapper is for avoid below error (maybe python error)
+			# close failed in file object destructor: sys.excepthook is missing lost sys.stderr
+			[[ -n $inputrc_filepath ]] && echo 'bind -f <(cat <(echo '$(cat "$inputrc_filepath" | local_base64encode)' | '$remote_base64decode'))'
+			# .bashrc
+			cat_if_exist "$HOME/.config/oressh/$host/.bashrc" || cat_if_exist "$HOME/.config/oressh/default/.bashrc"
 		} | local_base64encode) \
 		" | $remote_base64decode)'"
+	# NOTE: ~/.inputrcはprocess substitutionで指定してはならない?最初に設定後にrmする分には問題なさそう
+	# bashに入ってから<()で入力する分には問題ない
+	# 			cat <(echo 'bind -f ~/github.com/oressh/.inputrc')
+	# 			cat <(echo 'bind -f <(echo '$(cat ~/dotfiles/.inputrc | local_base64encode)' | '$remote_base64decode')')
 }
 
 oressh $1
